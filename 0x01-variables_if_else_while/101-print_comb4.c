e8131590e491b6dfd0874eae48e96ad1a540204b
Ibrahim Ibrahim <kaydemyrtle@gmail.com>
2022-10-14 08:01:06 -0700

    Commit

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -16,23 +16,15 @@
 int main(void)
 
 {
-
 	int d, p, q;
-
-
 
 	for (d = '0'; d < '9'; d++)
 
 	{
-
 		for (p = d + 1; p <= '9'; p++)
-
 		{
-
 			for (q = p + 1; q <= '9'; q++)
-
 			{
-
 				if ((p != d) != q)
 
 				{
@@ -43,29 +35,19 @@ int main(void)
 
 					putchar(q);
 
-
-
 					if (d == '7' && p == '8')
 
 						continue;
 
-
-
 					putchar(',');
 
 					putchar(' ');
-
 				}
-
 			}
-
 		}
-
 	}
 
 	putchar('\n');
-
-
 
 	return (0);
 

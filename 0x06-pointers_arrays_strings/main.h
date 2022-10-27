e7300daa04276a7cc2a2092d8bb549e31a9c4e1c
Ibrahim Ibrahim <kaydemyrtle@gmail.com>
2022-10-27 07:27:56 -0700

    Commit

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -1,3 +1,9 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+#include <stdlib.h>
+
 int _putchar(char c);
 char *_strcat(char *dest, char *src);
 char *_strncat(char *dest, char *src, int n);
@@ -9,3 +15,9 @@ char *cap_string(char *s);
 char *leet(char *s);
 char *rot13(char *);
 void print_number(int n);
+char *infinite_add(char *n1, char *n2, char *r, int size_r);
+void print_buffer(char *b, int size);
+void carry_from_prev(char *b, int i);
+char *find_str_end(char *s);
+
+#endif
